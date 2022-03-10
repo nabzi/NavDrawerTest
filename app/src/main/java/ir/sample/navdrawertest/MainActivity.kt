e631497817d81cc1acc126bd242c7cb7607a45d0
nabzi <zeinab.modir@gmail.com>
2022-03-10 14:47:30 +0330

    setup actionbar with navColntroller

--- app/src/main/java/ir/sample/navdrawertest/MainActivity.kt
+++ app/src/main/java/ir/sample/navdrawertest/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
@@ -20,5 +21,6 @@ class MainActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
