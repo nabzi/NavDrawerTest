c2939609fdd376b07badbecb0bcf335eda272eb7
nabzi <zeinab.modir@gmail.com>
2022-03-10 15:37:49 +0330

    changes to fix drawer open destinations

--- app/src/main/java/ir/sample/navdrawertest/MainActivity.kt
+++ app/src/main/java/ir/sample/navdrawertest/MainActivity.kt
@@ -2,10 +2,14 @@ package ir.sample.navdrawertest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
@@ -18,9 +22,12 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
         var drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+        val appBarConfiguration = AppBarConfiguration( setOf(
+            R.id.fragmentHome, R.id.help_fragment
+        ), drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
+
     }
 }
