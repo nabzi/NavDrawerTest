3c42a90aa2f15bfde43c8c41dd025c55066fa93c
nabzi <zeinab.modir@gmail.com>
2022-03-10 16:04:34 +0330

    add onSupportNAvigationUp

--- app/src/main/java/ir/sample/navdrawertest/MainActivity.kt
+++ app/src/main/java/ir/sample/navdrawertest/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -15,19 +16,29 @@ import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var  appBarConfiguration : AppBarConfiguration
+    lateinit var navController : NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        navController = navHostFragment.navController
         var drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout)
-        val appBarConfiguration = AppBarConfiguration( setOf(
-            R.id.fragmentHome, R.id.help_fragment
-        ), drawerLayout)
+//        appBarConfiguration = AppBarConfiguration( setOf(
+//            R.id.fragmentHome, R.id.help_fragment
+//        ), drawerLayout)
+        appBarConfiguration = AppBarConfiguration( navController.graph, drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
         findViewById<NavigationView>(R.id.nav_view)
             .setupWithNavController(navController)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
